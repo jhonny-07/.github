38e807cab9b07aa14f28daeec671496ebfe380b3
jhonny-07 <36981473+jhonny-07@users.noreply.github.com>
2020-09-04 21:43:02 -0300

    Add files via upload

---
+++ Dhow_ProjectActivity.java
@@ -0,0 +1,17 @@
+package Dhow_Project;
+
+import android.app.*;
+import android.os.*;
+import android.view.*;
+import android.widget.*;
+
+public class Dhow_ProjectActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+	{
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
